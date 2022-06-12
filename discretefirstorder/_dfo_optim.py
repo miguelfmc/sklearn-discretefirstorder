66d27993b1b0000e823880f337491880db04e1b1
Miguel FMC <miguelfm95@gmail.com>
2022-06-12 12:01:23 -0700

    Renamed _solve_dfo and implemented _threshold function

--- discretefirstorder/_dfo_optim.py
+++ discretefirstorder/_dfo_optim.py
@@ -5,5 +5,14 @@ Implementation of the discrete first-order method for subset selection
 import numpy as np
 
 
-def _plain_dfo():
+def _threshold(arr, k):
+    """Threshold array to keep top k largest elements
+    """
+    idx = np.argpartition(np.abs(arr), -k)[-k:]
+    result = np.zeros_like(arr)
+    result[idx] = arr[idx]
+    return result
+
+
+def _solve_dfo():
     pass
