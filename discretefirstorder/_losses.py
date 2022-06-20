1ac21cd3aed235002c7213883a92b0f3eb3d24f0
Miguel FMC <miguelfm95@gmail.com>
2022-06-19 19:41:09 -0700

    Created _losses.py module

---
+++ discretefirstorder/_losses.py
@@ -0,0 +1,28 @@
+"""
+Implementation of some basic losses and their gradients
+"""
+
+import numpy as np
+
+
+class MSELoss:
+    # TODO implement MSE loss and test it
+    def __init__(self):
+        pass
+
+    def loss(self, coef, X, y):
+        pass
+
+    def gradient(self, coef, X, y):
+        pass
+
+
+class LogLoss:
+    def __init__(self):
+        pass
+
+    def loss(self, coef, X, y):
+        pass
+
+    def gradient(self, coef, X, y):
+        pass
