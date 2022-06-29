62a67c9331d84c8ce3d1c12ba2201585a9163146
Miguel FMC <miguelfm95@gmail.com>
2022-06-28 23:31:08 -0700

    Added assertions for default parameters of DFORgressor

--- discretefirstorder/tests/test_discrete_first_order.py
+++ discretefirstorder/tests/test_discrete_first_order.py
@@ -16,8 +16,15 @@ def data():
 
 def test_dfo_regressor(data):
     reg = DFORegressor()
-    assert reg.demo_param == 'demo_param'
-    assert ...
+    assert reg.loss == "mse"
+    assert reg.learning_rate == "auto"
+    assert reg.k == 5
+    assert reg.polish == True
+    assert reg.n_runs == 50
+    assert reg.max_iter == 100
+    assert reg.tol == 1e-3
+    assert reg.fit_intercept == False
+    assert reg.normalize == False
 
     reg.fit(*data)
     assert hasattr(reg, 'is_fitted_')
