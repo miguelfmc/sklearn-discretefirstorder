67c10a9ee89a83d21d8537e92422c183f8d5ce0d
Miguel FMC <miguelfm95@gmail.com>
2022-07-07 22:24:39 -0700

    Distname and description

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python
-"""A template for scikit-learn compatible packages."""
+"""A scikit-learn compatible discrete first-order method
+for subset selection."""
 
 import codecs
 import os
@@ -7,59 +8,61 @@ import os
 from setuptools import find_packages, setup
 
 # get __version__ from _version.py
-ver_file = os.path.join('discretefirstorder', '_version.py')
+ver_file = os.path.join("discretefirstorder", "_version.py")
 with open(ver_file) as f:
     exec(f.read())
 
-DISTNAME = 'sklearn-firstordersubset'
-DESCRIPTION = 'A template for scikit-learn compatible packages.'
-with codecs.open('README.rst', encoding='utf-8-sig') as f:
+DISTNAME = "sklearn-discretefirstorder"
+DESCRIPTION = "A scikit-learn compatible discrete first-order method for subset selection."
+with codecs.open("README.rst", encoding="utf-8-sig") as f:
     LONG_DESCRIPTION = f.read()
-MAINTAINER = 'V. Birodkars, G. Lemaitre'
-MAINTAINER_EMAIL = 'vighneshbirodkar@nyu.edu, g.lemaitre58@gmail.com'
-URL = 'https://github.com/scikit-learn-contrib/project-template'
-LICENSE = 'new BSD'
-DOWNLOAD_URL = 'https://github.com/scikit-learn-contrib/project-template'
+MAINTAINER = "M. Fernandez-Montes"
+MAINTAINER_EMAIL = "miguel_fmontes@berkeley.edu"
+URL = ""
+LICENSE = "new BSD"
+DOWNLOAD_URL = ""
 VERSION = __version__
-INSTALL_REQUIRES = ['numpy', 'scipy', 'scikit-learn']
-CLASSIFIERS = ['Intended Audience :: Science/Research',
-               'Intended Audience :: Developers',
-               'License :: OSI Approved',
-               'Programming Language :: Python',
-               'Topic :: Software Development',
-               'Topic :: Scientific/Engineering',
-               'Operating System :: Microsoft :: Windows',
-               'Operating System :: POSIX',
-               'Operating System :: Unix',
-               'Operating System :: MacOS',
-               'Programming Language :: Python :: 2.7',
-               'Programming Language :: Python :: 3.5',
-               'Programming Language :: Python :: 3.6',
-               'Programming Language :: Python :: 3.7']
+INSTALL_REQUIRES = ["numpy", "scipy", "scikit-learn"]
+CLASSIFIERS = [
+    "Intended Audience :: Science/Research",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved",
+    "Programming Language :: Python",
+    "Topic :: Software Development",
+    "Topic :: Scientific/Engineering",
+    "Operating System :: Microsoft :: Windows",
+    "Operating System :: POSIX",
+    "Operating System :: Unix",
+    "Operating System :: MacOS",
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+]
 EXTRAS_REQUIRE = {
-    'tests': [
-        'pytest',
-        'pytest-cov'],
-    'docs': [
-        'sphinx',
-        'sphinx-gallery',
-        'sphinx_rtd_theme',
-        'numpydoc',
-        'matplotlib'
-    ]
+    "tests": ["pytest", "pytest-cov"],
+    "docs": [
+        "sphinx",
+        "sphinx-gallery",
+        "sphinx_rtd_theme",
+        "numpydoc",
+        "matplotlib",
+    ],
 }
 
-setup(name=DISTNAME,
-      maintainer=MAINTAINER,
-      maintainer_email=MAINTAINER_EMAIL,
-      description=DESCRIPTION,
-      license=LICENSE,
-      url=URL,
-      version=VERSION,
-      download_url=DOWNLOAD_URL,
-      long_description=LONG_DESCRIPTION,
-      zip_safe=False,  # the package can run out of an .egg file
-      classifiers=CLASSIFIERS,
-      packages=find_packages(),
-      install_requires=INSTALL_REQUIRES,
-      extras_require=EXTRAS_REQUIRE)
+setup(
+    name=DISTNAME,
+    maintainer=MAINTAINER,
+    maintainer_email=MAINTAINER_EMAIL,
+    description=DESCRIPTION,
+    license=LICENSE,
+    url=URL,
+    version=VERSION,
+    download_url=DOWNLOAD_URL,
+    long_description=LONG_DESCRIPTION,
+    zip_safe=False,  # the package can run out of an .egg file
+    classifiers=CLASSIFIERS,
+    packages=find_packages(),
+    install_requires=INSTALL_REQUIRES,
+    extras_require=EXTRAS_REQUIRE,
+)
