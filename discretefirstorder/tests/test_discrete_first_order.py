77e438d4c59f96ac783f2d36f10a6f49702115b7
Miguel FMC <miguelfm95@gmail.com>
2022-07-09 10:51:15 -0700

    Removed unnecessary imports, added docstrings, fixed boolean comparisons

--- discretefirstorder/tests/test_discrete_first_order.py
+++ discretefirstorder/tests/test_discrete_first_order.py
@@ -1,30 +1,31 @@
-import pytest
-import numpy as np
+"""
+Test estimators
+"""
 
+import pytest
 from sklearn.datasets import load_iris
-from numpy.testing import assert_array_equal
-from numpy.testing import assert_allclose
 
 from discretefirstorder import DFORegressor
 
 
 @pytest.fixture
 def data():
+    """Load Iris dataset for testing"""
     return load_iris(return_X_y=True)
 
 
 def test_dfo_regressor(data):
+    """Test DFORegressor"""
     reg = DFORegressor()
     assert reg.loss == "mse"
     assert reg.learning_rate == "auto"
     assert reg.k == 3
-    assert reg.polish == True
+    assert reg.polish is True
     assert reg.n_runs == 50
     assert reg.max_iter == 100
     assert reg.tol == 1e-3
-    assert reg.fit_intercept == False
-    assert reg.normalize == False
+    assert reg.fit_intercept is False
+    assert reg.normalize is False
 
     reg.fit(*data)
-    assert hasattr(reg, 'is_fitted_')
-
+    assert hasattr(reg, "is_fitted_")
