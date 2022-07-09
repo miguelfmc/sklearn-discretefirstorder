dc8c0c1d470a96db3e0420297a89cc44c46dbdda
Miguel FMC <miguelfm95@gmail.com>
2022-07-09 10:48:21 -0700

    Removed DFOClassifier

--- discretefirstorder/tests/test_common.py
+++ discretefirstorder/tests/test_common.py
@@ -1,14 +1,14 @@
-import pytest
+"""
+Test common
+"""
 
+import pytest
 from sklearn.utils.estimator_checks import check_estimator
 
-from discretefirstorder import DFOClassifier
 from discretefirstorder import DFORegressor
 
 
-@pytest.mark.parametrize(
-    "estimator",
-    [DFORegressor(), DFOClassifier()]
-)
+@pytest.mark.parametrize("estimator", [DFORegressor()])
 def test_all_estimators(estimator):
+    """Standard scikit-learn estimator checks"""
     return check_estimator(estimator)
