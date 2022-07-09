6a91bf8f1e08ca706c45df9209720b5f947f0625
Miguel FMC <miguelfm95@gmail.com>
2022-07-09 11:46:59 -0700

    Test for _dfo_optim.py

---
+++ discretefirstorder/tests/test_dfo_optim.py
@@ -0,0 +1,17 @@
+"""
+Test DFO algorithm and functions
+"""
+
+import pytest
+
+
+def test_threshold():
+    pass
+
+
+def test_calculate_learning_rate():
+    pass
+
+
+def test_solve_dfo():
+    pass
