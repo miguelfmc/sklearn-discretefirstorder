de505790166b8b92e94ff70df8911ac32eb6f26c
Miguel FMC <miguelfm95@gmail.com>
2022-07-17 18:00:41 -0700

    Test threshold function

--- discretefirstorder/tests/test_dfo_optim.py
+++ discretefirstorder/tests/test_dfo_optim.py
@@ -2,16 +2,23 @@
 Test DFO algorithm and functions
 """
 
-import pytest
+from discretefirstorder._dfo_optim import _threshold
 
 
 def test_threshold():
-    pass
+    """Test _threshold"""
+    import numpy as np
+
+    arr = np.array([-10, 5, 1, 3, -4, 8, 2])
+    out = np.array([-10, 5, 0, 0, 0, 8, 0])
+    assert np.array_equal(_threshold(arr, 3), out)
 
 
 def test_calculate_learning_rate():
+    """Test _calculate_learning_rate"""
     pass
 
 
 def test_solve_dfo():
+    """Test _solve_dfo"""
     pass
