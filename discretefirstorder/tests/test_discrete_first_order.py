f2ce4f189abbd50e0abd4371d134a3e8404fdcad
Miguel FMC <miguelfm95@gmail.com>
2022-07-17 18:25:07 -0700

    Test proper params init

--- discretefirstorder/tests/test_discrete_first_order.py
+++ discretefirstorder/tests/test_discrete_first_order.py
@@ -24,6 +24,29 @@ def test_dfo_regressor_default():
     assert reg.n_runs == 50
     assert reg.max_iter == 100
     assert reg.tol == 1e-3
+    assert reg.fit_intercept is True
+    assert reg.normalize is True
+
+
+def test_dfo_regressor():
+    """Test DFORegressor params"""
+    reg = DFORegressor(
+        learning_rate=0.001,
+        k=5,
+        polish=False,
+        n_runs=25,
+        max_iter=50,
+        tol=1e-4,
+        fit_intercept=False,
+        normalize=False,
+    )
+    assert reg.loss == "mse"
+    assert reg.learning_rate == 0.001
+    assert reg.k == 5
+    assert reg.polish is False
+    assert reg.n_runs == 25
+    assert reg.max_iter == 50
+    assert reg.tol == 1e-4
     assert reg.fit_intercept is False
     assert reg.normalize is False
 
