e92b4b3caabdba279c05e0644fe3397cc37b1623
Miguel FMC <miguelfm95@gmail.com>
2022-08-14 20:48:28 -0700

    Added _preprocess_data to __all__

--- discretefirstorder/__init__.py
+++ discretefirstorder/__init__.py
@@ -1,6 +1,7 @@
 from ._discrete_first_order import BaseDFO
 from ._discrete_first_order import DFORegressor
+from ._discrete_first_order import _preprocess_data
 
 from ._version import __version__
 
-__all__ = ['BaseDFO', 'DFORegressor', '__version__']
+__all__ = ["BaseDFO", "DFORegressor", "_preprocess_data", "__version__"]
