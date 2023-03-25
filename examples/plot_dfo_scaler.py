8c8f4557bdb404f45de5aebe26a2b87617df2009
Miguel FMC <miguelfm95@gmail.com>
2023-03-25 11:05:23 -0700

    Added scaling example for docs

---
+++ examples/plot_dfo_scaler.py
@@ -0,0 +1,10 @@
+"""
+========================================
+Scaling input data for the DFO Regressor
+========================================
+
+DFORegressor() can normalize the data internally or it can be used
+in conjunction with a sklearn scaler e.g. StandardScaler.
+
+"""
+# TODO create scaler example
