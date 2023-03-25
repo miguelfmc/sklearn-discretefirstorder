51ff48b0c0f961bd5da722fcea231790669841ef
Miguel FMC <miguelfm95@gmail.com>
2023-03-25 11:02:05 -0700

    Binary cross-entropy loss

--- discretefirstorder/_losses.py
+++ discretefirstorder/_losses.py
@@ -3,6 +3,7 @@ Implementation of some basic losses and their gradients
 """
 
 import numpy as np
+from scipy.special import expit
 
 
 class MSELoss:
@@ -21,6 +22,7 @@ class MSELoss:
     >>> loss.gradient(coef, X, y)
     array([11,  4])
     """
+
     def __init__(self):
         pass
 
@@ -66,3 +68,67 @@ class MSELoss:
         """
         return -X.T @ (y - X @ coef)
 
+
+class BinaryCrossEntropyLoss:
+    """Implementation of binary cross entropy loss
+        and its gradient, for a linear model.
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> y = np.array([1, 0, 0, 1])
+    >>> X = np.array([[1, 1], [-1, 0], [1, -1], [0, 1]])
+    >>> coef = np.array([1, 1])
+    >>> loss = BinaryCrossEntropyLoss()
+    >>> loss.loss(coef, X, y)
+    1.4465985666393635
+    >>> loss.gradient(coef, X, y)
+    array([ 0.11185566, -0.88814434])
+    """
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def loss(coef, X, y):
+        """Calculate binary cross entropy loss for a linear model
+            given the coefficients, data and target.
+
+        Parameters
+        ----------
+        coef : array-like of shape (n_features,)
+            coefficient vector.
+        X : ndarray of shape (n_samples, n_features)
+            training data.
+        y : array-like of shape (n_samples,)
+            target values.
+
+        Returns
+        -------
+        loss_value : float
+            loss value.
+        """
+        y_pred = expit(X @ coef)
+        return -np.sum(y * np.log(y_pred) + (1 - y) * np.log(1 - y_pred))
+
+    @staticmethod
+    def gradient(coef, X, y):
+        """Calculate binary cross entropy loss gradient for a linear model
+            given the coefficients, data and target.
+
+        Parameters
+        ----------
+        coef : array-like of shape (n_features,)
+            coefficient vector.
+        X : ndarray of shape (n_samples, n_features)
+            training data.
+        y : array-like of shape (n_samples,)
+            target values.
+
+        Returns
+        -------
+        gradient : ndarray of shape (n_features,)
+            gradient of the binary cross entropy loss w.r.t. coefficient vector.
+        """
+        y_pred = expit(X @ coef)
+        return X.T @ (y_pred - y)
