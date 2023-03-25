0a4c062a8f6855ece1cbea64859f249ea92659bb
Miguel FMC <miguelfm95@gmail.com>
2023-03-25 11:02:48 -0700

    Added DFOClassifier to __all__

--- discretefirstorder/__init__.py
+++ discretefirstorder/__init__.py
@@ -1,5 +1,5 @@
 from ._discrete_first_order import BaseDFO
-from ._discrete_first_order import DFORegressor
+from ._discrete_first_order import DFORegressor, DFOClassifier
 
 from ._dfo_optim import _threshold, _solve_dfo
 
@@ -8,6 +8,7 @@ from ._version import __version__
 __all__ = [
     "BaseDFO",
     "DFORegressor",
+    "DFOClassifier",
     "_solve_dfo",
     "_threshold",
     "__version__",
